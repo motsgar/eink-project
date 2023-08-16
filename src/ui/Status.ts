eff54ee02b0748b9978efb033a060a9db65b7903
Juho Röyskö <roysko.juho@gmail.com>
2023-08-16 03:03:23 +0300

    Update weather icons and improve status module

--- src/ui/Status.ts
+++ src/ui/Status.ts
@@ -1,11 +1,19 @@
-import { Canvas, createCanvas } from 'canvas';
+import { Canvas, Image, createCanvas, loadImage } from 'canvas';
 import { EInkModule } from './EInkModule';
 import { weatherData } from './weatherData';
 
 export class Status extends EInkModule {
+    private sunriseIcon?: Image;
+    private sunsetIcon?: Image;
+
     constructor() {
         super();
-        this.readyPromise = Promise.all([weatherData.readyPromise]);
+        this.readyPromise = Promise.all([this.loadIcons(), weatherData.readyPromise]);
+    }
+
+    private async loadIcons(): Promise<void> {
+        this.sunriseIcon = await loadImage('resources/icons/sunrise.svg');
+        this.sunsetIcon = await loadImage('resources/icons/sunset.svg');
     }
 
     draw(width: number, height: number): Canvas {
@@ -13,13 +21,25 @@ export class Status extends EInkModule {
         const ctx = canvas.getContext('2d');
 
         const observation = weatherData.weatherData?.observation;
-        if (observation === undefined) throw new Error("Weather data hasn't been initialized");
+        const sunInfo = weatherData.weatherData?.sunInfo;
+        const warnings = weatherData.weatherData?.warnings;
+        if (warnings === undefined || sunInfo === undefined || observation === undefined) {
+            throw new Error("Weather data hasn't been initialized");
+        }
+        if (this.sunriseIcon === undefined || this.sunsetIcon === undefined) {
+            throw new Error("Status icons haven't been initialized");
+        }
 
+        // Main clock + date
         ctx.font = '300 120pt sans-serif';
         ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
+        ctx.textBaseline = 'top';
         ctx.fillStyle = 'black';
-        ctx.fillText(new Date().toTimeString().substring(0, 5), width / 2, height / 2 - 25);
+        let yPos = height / 2 - 150;
+        const timeText = new Date().toTimeString().substring(0, 5);
+        ctx.fillText(timeText, width / 2, yPos);
+        const timeMeasureText = ctx.measureText(timeText);
+        yPos += timeMeasureText.actualBoundingBoxAscent + timeMeasureText.actualBoundingBoxDescent + 60;
 
         ctx.font = '22pt sans-serif';
         const options: Intl.DateTimeFormatOptions = {
@@ -28,22 +48,70 @@ export class Status extends EInkModule {
             month: 'long',
             day: 'numeric',
         };
-        ctx.fillText(new Date().toLocaleDateString('en-US', options), width / 2, height / 2 + 85);
+        ctx.fillText(new Date().toLocaleDateString('en-US', options), width / 2, yPos);
 
-        ctx.font = '25pt Sherisans-serifff';
+        ctx.font = '25pt sans-serif';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'bottom';
+        ctx.fillText(sunInfo.suntxt, width / 2, height - 10);
+        const textWidth = ctx.measureText(sunInfo.suntxt).width;
+        const iconSize = 40;
+        ctx.drawImage(
+            this.sunriseIcon,
+            width / 2 - textWidth / 2 - iconSize - 10,
+            height - 10 - iconSize,
+            iconSize,
+            iconSize,
+        );
+        // iconSize -= 4;
+        ctx.drawImage(this.sunsetIcon, width / 2 + textWidth / 2 + 10, height - 10 - iconSize, iconSize, iconSize);
+
+        // Other text
+        ctx.font = '25pt sans-serif';
+        const padding = 30;
+
         ctx.textAlign = 'left';
         ctx.textBaseline = 'top';
-        ctx.fillText('25.6°C', 10, 10);
-
-        ctx.textAlign = 'right';
-        ctx.fillText('713 ppm', width - 10, 10);
+        let text = ['25.6°C', '713 ppm'];
+        yPos = 10;
+        for (const line of text) {
+            ctx.fillText(line, 10, yPos);
+            const measureText = ctx.measureText(line);
+            yPos += measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent + padding;
+        }
 
         ctx.textAlign = 'left';
         ctx.textBaseline = 'bottom';
-        ctx.fillText(`${observation.temperature}°C`, 10, height - 10);
+        text = ['21 %', '1003 atm'];
+        yPos = height - 10;
+        for (const line of text.reverse()) {
+            ctx.fillText(line, 10, yPos);
+            const measureText = ctx.measureText(line);
+            yPos -= measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent + padding;
+        }
 
         ctx.textAlign = 'right';
-        ctx.fillText(`${observation.windSpeedMS} m/s`, width - 10, height - 10);
+        ctx.textBaseline = 'top';
+        text = [`${observation.temperature}°C`, `${observation.windSpeedMS} m/s`];
+        yPos = 10;
+        for (const line of text) {
+            ctx.fillText(line, width - 10, yPos);
+            const measureText = ctx.measureText(line);
+            yPos += measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent + padding;
+        }
+
+        ctx.textAlign = 'right';
+        ctx.textBaseline = 'bottom';
+        text = Object.entries(warnings)
+            .filter((warning) => warning[1])
+            .map((warning) => warning[0]);
+        if (text.length === 0) text = ['No warnings'];
+        yPos = height - 10;
+        for (const line of text.reverse()) {
+            ctx.fillText(line, width - 10, yPos);
+            const measureText = ctx.measureText(line);
+            yPos -= measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent + padding;
+        }
 
         return canvas;
     }
