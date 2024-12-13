6ae94514e093138f8baf900af7684e2e5c66da14
Juho Röyskö <roysko.juho@gmail.com>
2024-12-13 22:33:40 +0200

    Add updated vite tsconfig and remove unused html files

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,11 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+
+/* eslint-disable require-unicode-regexp */
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [react()],
     server: {
         port: 5000,
         proxy: {
